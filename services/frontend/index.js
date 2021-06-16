382567c48c93e55f50d0bde814aaf4eb24adb3f7
agbriela <gabriela.lima13423@gmail.com>
2021-06-16 15:07:50 -0300

    Implementa controle de estoque dos livros

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -1,3 +1,25 @@
+const stock = [{'id':1,'quantity':10},{'id':2,'quantity':10},{'id':3,'quantity':10}]
+
+function addBooks(){
+    for(i = 0; i< stock.length; i++){
+        stock[i].quantity = 5
+    }
+}
+
+function controlestock(id){
+    if(stock[id-1].quantity == 0){
+        stock[id-1].quantity = 'Produto Indisponível'   
+    }else{
+        stock[id-1].quantity = stock[id-1].quantity - 1  
+        if(stock[id-1].quantity == 0){
+            stock[id-1].quantity = 'Produto Indisponível'   
+        }
+    }
+    const books = document.querySelector('.books');
+    books.innerHTML = ''
+    start()
+}
+
 function newBook(book) {
     const div = document.createElement('div');
     div.className = 'column is-4';
@@ -16,7 +38,7 @@ function newBook(book) {
                 <div class="content book" data-id="${book.id}">
                     <div class="book-meta">
                         <p class="is-size-4">R$${book.price.toFixed(2)}</p>
-                        <p class="is-size-6">Disponível em estoque: 5</p>
+                        <p class="is-size-6 stock">Disponível em estoque: ${stock[book.id-1].quantity}</p>
                         <h4 class="is-size-3 title">${book.name}</h4>
                         <p class="subtitle">${book.author}</p>
                     </div>
@@ -28,11 +50,11 @@ function newBook(book) {
                             <a class="button button-shipping is-info" data-id="${book.id}"> Calcular Frete </a>
                         </div>
                     </div>
-                    <button class="button button-buy is-success is-fullwidth">Comprar</button>
+                    <button class="button button-buy is-success is-fullwidth" data-id="${book.id}">Comprar</button>
                 </div>
             </div>
         </div>`;
-    return div;
+        return div;
 }
 
 function calculateShipping(id, cep) {
@@ -53,6 +75,11 @@ function calculateShipping(id, cep) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    addBooks()
+    start()
+});
+
+function start(){
     const books = document.querySelector('.books');
 
     fetch('http://localhost:3000/products')
@@ -78,6 +105,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 document.querySelectorAll('.button-buy').forEach((btn) => {
                     btn.addEventListener('click', (e) => {
+                        const id = e.target.getAttribute('data-id');
+                        controlestock(id)
                         swal('Compra de livro', 'Sua compra foi realizada com sucesso', 'success');
                     });
                 });
@@ -87,4 +116,5 @@ document.addEventListener('DOMContentLoaded', function () {
             swal('Erro', 'Erro ao listar os produtos', 'error');
             console.error(err);
         });
-});
+}
+
